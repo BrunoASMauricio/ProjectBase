f001268a41cc64bc189b54aa6b1e7e4dbd1e3480
BrunoASMauricio <brunoasmauricio@gmail.com>
2024-02-17 17:31:49 +0000

    Templates: Removed arguments from tests' main
    Signed-off-by: BrunoASMauricio <brunoasmauricio@gmail.com>

--- templates/examples/exampleTest.cpp
+++ templates/examples/exampleTest.cpp
@@ -2,7 +2,7 @@
 
 #include "exampleHeader.hpp"
 
-int main(int argc, char* argv[]){
+int main(){
     printf("Template built-in test for repository testproject1\n");
     vCallExample();
     return 0;
