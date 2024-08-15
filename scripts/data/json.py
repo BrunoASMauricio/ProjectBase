793b4377ddac16a62a83716948cc5b9e49ba8a06
BrunoASMauricio <brunoasmauricio@gmail.com>
2024-08-15 19:02:43 +0100

    Add top level check for getting url
    Also clean up style a bit

--- scripts/data/json.py
+++ scripts/data/json.py
@@ -1,34 +1,34 @@
 import traceback
 import json
 
-def load_json_file(Path, ErrorValue=None, variable_substitutions={}):
+def load_json_file(path, error_value=None, variable_substitutions={}):
     """
-    Returns the json from the given file, or ErrorValue in cas of an error
+    Returns the json from the given file, or error_value in cas of an error
     Parameters
     ----------
     file : string
         The target file path
-    ErrorValue : Object
+    error_value : Object
         The object returned in case there is an error loading the json file
         If Object is None, the exception is thrown
     """
     try:
-        with open(Path, 'r') as File:
-            JsonData = json.load(File)
+        with open(path, 'r') as File:
+            json_data = json.load(File)
 
         for variable_name in variable_substitutions:
-            JsonData = JsonData.replace("$$" + variable_name + "$$", variable_substitutions[variable_name])
+            json_data = json_data.replace("$$" + variable_name + "$$", variable_substitutions[variable_name])
 
-        if JsonData == None:
+        if json_data == None:
             raise Exception("Null json data")
 
-        return JsonData
+        return json_data
 
     except Exception as Ex:
-        if ErrorValue == None:
-            raise Exception("Could not load json from file " + Path + " " + traceback.format_exc())
-    return ErrorValue
+        if error_value == None:
+            raise Exception("Could not load json from file " + path + " " + traceback.format_exc())
+    return error_value
 
-def dump_json_file(JsonData, Path):
-    with open(Path, 'w') as file:
-        json.dump(JsonData, file)
+def dump_json_file(json_data, path):
+    with open(path, 'w') as file:
+        json.dump(json_data, file)
