d4d9931e2ec0848a938b22529fc73cfc1c0605df
BrunoASMauricio <brunoasmauricio@gmail.com>
2024-10-04 17:17:57 +0100

    fix: Removed executable printing when running all test options

--- scripts/menus/run.py
+++ scripts/menus/run.py
@@ -4,6 +4,6 @@ from processes.run_exec import run_single_test, run_single_executable, run_all_t
 RunMenu = Menu("Run Menu")
 
 RunMenu.add_callback_entry("Run project executable", run_single_executable)
+RunMenu.add_callback_entry("Run single test", run_single_test)
 RunMenu.add_callback_entry("Run all tests", run_all_tests)
 RunMenu.add_callback_entry("Run all tests (with valgrind)", run_all_tests_on_valgrind)
-RunMenu.add_callback_entry("Run single test", run_single_test)
