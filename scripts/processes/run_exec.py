32b16f91d2360f425f992886b3e9352cca8b1eb2
BrunoASMauricio <brunoasmauricio@gmail.com>
2025-02-26 19:34:04 +0000

    Improve phrasing

--- scripts/processes/run_exec.py
+++ scripts/processes/run_exec.py
@@ -136,7 +136,7 @@ def execute_menu(PathToScan):
                 print("Keyboard Interrupt")
 
         except KeyboardInterrupt:
-            print("\nCtrl+C exits running operations. Press Ctrl+D to back out of ProjectBase")
+            print("\nCtrl+C interrupts running operations and enter goes to the previous menu. Press Ctrl+D to back out of ProjectBase")
             continue
         except EOFError:
             break
