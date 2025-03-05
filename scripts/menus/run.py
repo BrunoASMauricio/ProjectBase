a6e74c37dd5686bc3ac2ccea899a325349344607
BrunoASMauricio <brunoasmauricio@gmail.com>
2025-03-05 15:49:15 +0000

    Style fix

--- scripts/menus/run.py
+++ scripts/menus/run.py
@@ -1,9 +1,9 @@
 from menus.menu import Menu
-from processes.run_exec import run_single_test, run_single_executable, run_all_tests, run_all_tests_on_valgrind
+from processes.run_exec import RunSingleTest, RunSingleExecutable, RunAllTests, RunAllTestsWithValgrind
 
 RunMenu = Menu("Run Menu")
 
-RunMenu.AddCallbackEntry("Run project executable", run_single_executable)
-RunMenu.AddCallbackEntry("Run single test", run_single_test)
-RunMenu.AddCallbackEntry("Run all tests", run_all_tests)
-RunMenu.AddCallbackEntry("Run all tests (with valgrind)", run_all_tests_on_valgrind)
+RunMenu.AddCallbackEntry("Run project executable", RunSingleExecutable)
+RunMenu.AddCallbackEntry("Run single test", RunSingleTest)
+RunMenu.AddCallbackEntry("Run all tests", RunAllTests)
+RunMenu.AddCallbackEntry("Run all tests (with valgrind)", RunAllTestsWithValgrind)
