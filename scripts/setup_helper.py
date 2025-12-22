65dcad5c7cff47b10f3d9623a65daaeb97cc6ffa
BrunoASMauricio <brunoasmauricio@gmail.com>
2025-12-22 01:57:38 +0000

    Improve setup helper

--- scripts/setup_helper.py
+++ scripts/setup_helper.py
@@ -1,23 +1,27 @@
 import os
 import sys
-from common import Abort, SetupTemplateScript, GetProjectPaths
-
+from data.common import Abort, SetupScript
+from data.paths import GetProjectPaths
 
 """
 This script provides any necessary operation for the build process, so the exact build
  system in used can be abstracted, and no build system dependency is required.
 """
 
+file_perms = {
+    # R/W
+    "data": 0o666,
+    # R/W/X
+    "exec": 0o777
+}
 
-"""
-
-"""
+path_types = ["tests", "executables"]
 def WrongUsage(MainMessage = ""):
     ErrorMessage  = MainMessage+"\n"
     ErrorMessage += "\nUsage:\n"
     ErrorMessage += "Arguments: "+sys.argv[0]+" <PathType> <FileType> <FilePath>\n"
-    ErrorMessage += '\tPathType: ["tests" | "executables"] - Target path\n'
-    ErrorMessage += '\tFileType: ["data" | "exec"] - Whether to make file executable\n'
+    ErrorMessage += f'\tPathType: {path_types} - Target path\n'
+    ErrorMessage += '\tFileType: {file_perms.keys()} - Whether to make file executable\n'
     ErrorMessage += '\tFilePath: Path to the original resource\n'
     ErrorMessage += "Received data:\n\t"+'\n\t'.join(sys.argv[1:])+"\n"
     ErrorMessage += "Cannot proceed"
@@ -33,18 +37,15 @@ if len(sys.argv) != 5:
 # Sane arguments
 _, ProjectName, PathType, FileType, FilePath = sys.argv
 
-FileTypePermissions = {
-    # R/W
-    "data": 0o666,
-    # R/W/X
-    "exec": 0o777
-}
+conditions = [
+    [len(FilePath) > 0, f"A proper file path must be provided: {FilePath}"],
+    [PathType in path_types, f"Only {path_types} are allowed as path type"],
+    [FileType in file_perms.keys(), f"Only {file_perms.keys()} permissions are allowed"],
+    [os.path.isfile(FilePath), f"File ({FilePath}) must exist"]
+]
 
-EverythingIsOk =                      PathType in ["tests", "executables"]
-EverythingIsOk = EverythingIsOk and ( FileType in FileTypePermissions.keys() )
-EverythingIsOk = EverythingIsOk and ( os.path.isfile(FilePath) )
-if EverythingIsOk != True:
-    WrongUsage("Arguments are not sane")
+if any(not sublist[0] for sublist in conditions):
+    WrongUsage(f"Arguments are not sane: {conditions}")
 
 # Correct project path
 ProjectPaths = GetProjectPaths(ProjectName)
@@ -52,9 +53,12 @@ if not os.path.isdir(ProjectPaths["project main"]):
     WrongUsage('Project "'+ProjectPaths["project main"]+'" does not exist')
 
 FileName = FilePath.split("/")[-1]
-TargetPath = ProjectPaths[PathType]+"/"+FileName
-SetupTemplateScript(FilePath, TargetPath)
 
-Permissions = FileTypePermissions[FileType]
+print(f"Setting up {FileName} as a {FileType} file in the {PathType} folder")
+
+TargetPath = ProjectPaths[PathType]+"/"+FileName
+SetupScript(FilePath, TargetPath)
+
+
+Permissions = file_perms[FileType]
 os.chmod(TargetPath, Permissions)
-# message("Setting up ${FILE_PATH} as a ${FILE_TYPE} file in the ${PATH_TYPE} folder")
