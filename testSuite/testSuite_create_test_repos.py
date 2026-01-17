a16bd4b2d0e4eccf92f6effa6ce01f29409cefef
BrunoASMauricio <brunoasmauricio@gmail.com>
2026-01-17 20:47:51 +0000

    Add error print to create test repos

--- testSuite/testSuite_create_test_repos.py
+++ testSuite/testSuite_create_test_repos.py
@@ -8,8 +8,11 @@ from PB_test_support import *
 from PB_test_base_repos import *
 
 def CreateTestRepos(branch):
-    repos = CreateBaseRepos()
-    for repo in repos:
-        print(f"Created repository: {repo.name} in {repo.bare_path} with a url of {repo.url}")
+    try:
+        repos = CreateBaseRepos()
+        for repo in repos:
+            print(f"Created repository: {repo.name} in {repo.bare_path} with a url of {repo.url}")
+    except Exception as ex:
+        print(f"Failed to create repos: {ex}")
 
 RunTests([CreateTestRepos])
